e373bd5cf605303c45cfc90e731f08b6b4d8fced
Roman <hawladar.roman@bjitgroup.com>
2019-11-28 09:59:10 +0600

    update config

---
+++ lib/login_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+/**
+ * Created by roman on 2019-11-28
+ * Copyright (c) 2019 bjit. All rights reserved.
+ * hawladar.roman@bjitgroup.com
+ * Last modified $file.lastModified
+ */
+class LoginScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
+  }
+
+}
